35b79f95d96bdb33af7bf0f6dece69d44ae12e15
mac <Kunal123>
2019-04-17 01:17:53 -0400

    checkout screen added.

--- app/src/main/java/com/goodcompany/group5_finalproject_w2019_mad3125/Activities/CheckoutActivity.java
+++ app/src/main/java/com/goodcompany/group5_finalproject_w2019_mad3125/Activities/CheckoutActivity.java
@@ -2,14 +2,69 @@ package com.goodcompany.group5_finalproject_w2019_mad3125.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.craftman.cardform.Card;
+import com.craftman.cardform.CardForm;
+import com.craftman.cardform.OnPayBtnClickListner;
 import com.goodcompany.group5_finalproject_w2019_mad3125.R;
 
-public class CheckoutActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CheckoutActivity extends BaseActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+        CardForm cardForm =  findViewById(R.id.card_form);
+        cardForm.setPayBtnClickListner(new OnPayBtnClickListner() {
+            @Override
+            public void onClick(Card card) {
+                //Your code here!! use card.getXXX() for get any card property
+                //for instance card.getName();
+            }
+        });
+
+        handleSpinner();
     }
+
+    private void handleSpinner() {
+
+        // Spinner element
+        Spinner spinner = (Spinner) findViewById(R.id.addressSpin);
+
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("59 First Gulf Blvd");
+        categories.add("B25 Peel Centre Drive");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+    }
+
 }
